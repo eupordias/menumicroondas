0f84889560ec14f787374b9b92cff4b3df8e2158
eupordias <eupordia@gmail.com>
2022-03-31 12:25:45 -0300

    DESCRIÇÕES ATUALIZADAS

--- menu.js
+++ menu.js
@@ -1,51 +1,51 @@
 function microondas(prato,tempo) {
     if (prato > 5) {
-        console.log('PRATO INEXISTENTE');
+        console.log('NÃO EXISTE PRATO');
     }
 if (prato == 1 && tempo < 10) {
-    console.log('TEMPO INSUFICIENTE!!!!!!');
+    console.log('TEMPO INSULFICIENTE');
 } else if (prato == 1 && (tempo >= 10 && tempo < 20)) {
-    console.log('A SUA PIPOCA ESTÁ PRONTA!!!!');
+    console.log('A SUA PIPOCA ESTÁ PRONTA!');
 } else if (prato == 1 && (tempo >= 20 && tempo < 30)) {
-    console.log('A SUA PIPOCA QUEIMOU!!!!!');
+    console.log('A SUA PIPOCA TORROU!');
 } else if (prato == 1 && tempo >= 30) {
-    console.log('KABUMMMMMM......');
+    console.log('EXPLODIU');
 }
 if (prato == 2 && tempo < 8) {
-    console.log('TEMPO INSUFICIENTE!!!!!!');
+    console.log('TEMPO INSUFICIENTE!');
 } else if (prato == 2 && (tempo >= 8 && tempo < 16)) {
-    console.log('MACARRÃO ESTÁ PRONTA!!!!');
+    console.log('MACARRÃO ESTÁ NO PONTO!');
 } else if (prato == 2 && (tempo >= 16 && tempo < 24)) {
-    console.log('MACARRÃO QUEIMOU!!!!!');
+    console.log('MACARRÃO QUEIMOU VEY');
 } else if (prato == 2 && tempo >= 24) {
-    console.log('KABUMMMMMM......');
+    console.log('FUDEU');
 }
 if (prato == 3 && tempo < 15) {
-    console.log('TEMPO INSUFICIENTE!!!!!!');
+    console.log('TEMPO INSUFICIENTE!');
 } else if (prato == 3 && (tempo >= 15 && tempo < 30)) {
-    console.log('CARNE ESTÁ PRONTA!!!!');
+    console.log('CARNE ESTÁ PRONTA!');
 } else if (prato == 3 && (tempo >= 30 && tempo < 90)) {
-    console.log('CARNE QUEIMOU!!!!!');
+    console.log('CARNE QUEIMOU!');
 } else if (prato == 3 && tempo >= 90) {
-    console.log('KABUMMMMMM......');
+    console.log('KABUMMMMMM..');
 }
 if (prato == 4 && tempo < 12) {
-    console.log('TEMPO INSUFICIENTE!!!!!!');
+    console.log('TEMPO INSUFICIENTE!');
 } else if (prato == 4 && (tempo >= 12 && tempo < 24)) {
-    console.log('FEIJÃO ESTÁ PRONTA!!!!');
+    console.log('FEIJÃO ESTÁ PRONTA!');
 } else if (prato == 4 && (tempo >= 24 && tempo < 36)) {
-    console.log('FEIJÃO QUEIMOU!!!!!');
+    console.log('FEIJÃO QUEIMOU!');
 } else if (prato == 4 && tempo >= 36) {
-    console.log('KABUMMMMMM......');
+    console.log('KABUMMMMMM.');
 }
 if (prato == 5 && tempo < 8) {
-    console.log('TEMPO INSUFICIENTE!!!!!!');
+    console.log('TEMPO INSUFICIENTE!');
 } else if (prato == 5 && (tempo >= 8 && tempo < 16)) {
-    console.log('BRIGADEIRO ESTÁ PRONTA!!!!');
+    console.log('BRIGADEIRO ESTÁ PRONTO!');
 } else if (prato == 5 && (tempo >= 16 && tempo < 24)) {
-    console.log('BRIGADEIRO QUEIMOU!!!!!');
+    console.log('BRIGADEIRO QUEIMOU!');
 } else if (prato == 5 && tempo >= 24) {
-    console.log('KABUMMMMMM......');
+    console.log('FUDEO......');
 }
 
 
